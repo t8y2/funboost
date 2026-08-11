2b866e55c8a036185a97b558fac29f139ccb50e4
ydf <ydf@nb.com>
2026-08-11 14:29:31 +0800

    commit

--- funboost/__init__.py
+++ funboost/__init__.py
@@ -13,7 +13,7 @@ set_frame_config这个模块的 use_config_form_funboost_config_module() 是核�
 这段注释说明和使用的用户无关,只和框架开发人员有关.
 '''
 
-__version__ = "54.8"
+__version__ = "54.9"
 
 from funboost.set_frame_config import show_frame_config
 
